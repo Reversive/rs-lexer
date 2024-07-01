7d5b5063af0e58aba9702508f578b1fe4ef8e6cf
Reversive <matiaspavan@gmail.com>
2024-07-01 14:35:29 -0300

    feat: single and compound operators

--- src/lexer.rs
+++ src/lexer.rs
@@ -38,11 +38,51 @@ impl<'a> Lexer<'a> {
         ));
     }
 
+    fn inmediate_next_match(&mut self, expected: char) -> bool {
+        if self.is_at_eof() {
+            return false;
+        }
+
+        if self.source.chars().nth(self.current_lexeme_offset).unwrap() != expected {
+            return false;
+        }
+
+        self.current_lexeme_offset += 1;
+        true
+    }
+
+    fn add_token_conditionally(
+        &mut self,
+        expected: char,
+        compound_type: TokenType,
+        single_type: TokenType,
+    ) {
+        let token_type = if self.inmediate_next_match(expected) {
+            compound_type
+        } else {
+            single_type
+        };
+        self.add_token(token_type);
+    }
+
     fn scan_token(&mut self) {
         let current_char = self.next_char();
         match current_char {
-            '(' => self.add_token(TokenType::LeftBrace),
-            _ => (),
+            '(' => self.add_token(TokenType::LeftParenthesis),
+            ')' => self.add_token(TokenType::RightParenthesis),
+            '{' => self.add_token(TokenType::LeftBrace),
+            '}' => self.add_token(TokenType::RightBrace),
+            ',' => self.add_token(TokenType::Comma),
+            '.' => self.add_token(TokenType::Dot),
+            '-' => self.add_token(TokenType::Minus),
+            '+' => self.add_token(TokenType::Plus),
+            ';' => self.add_token(TokenType::Semicolon),
+            '*' => self.add_token(TokenType::Star),
+            '!' => self.add_token_conditionally('=', TokenType::BangEqual, TokenType::Bang),
+            '=' => self.add_token_conditionally('=', TokenType::EqualEqual, TokenType::Equal),
+            '<' => self.add_token_conditionally('=', TokenType::LessEqual, TokenType::Less),
+            '>' => self.add_token_conditionally('=', TokenType::GreaterEqual, TokenType::Greater),
+            _ => println!("Unsupported character at line {}.", self.line),
         }
     }
 
